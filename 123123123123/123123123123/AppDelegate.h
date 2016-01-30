c8abf8cd71a18078a57ad3a5e03eb98a35aae34f
lanouhn <380347653@qq.com>
2016-01-30 09:52:57 +0800

    123

---
+++ 123123123123/123123123123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123123123123
+//
+//  Created by lanouhn on 16/1/30.
+//  Copyright © 2016年 刘永琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
